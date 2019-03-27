7e0be8983ea94477322abc81970afe5578ee33a4
polodev <polodev10@gmail.com>
2019-03-28 02:30:37 +0600

    update

--- resources/views/omc/index.blade.php
+++ resources/views/omc/index.blade.php
@@ -50,16 +50,14 @@
                 <h2 class="mt-2">Here: </h2>
                 <p>Age 23 will fetch rows -> id 1, 6, 9</p>
                 <p>Age 78 will fetch rows -> id 8, 12</p>
-                <p>Question: How can query using eloquent or mysql </p>
+                <p>I will search database by age like 23, 78</p>
+                <p>Question: How can query in eloquent or mysql in this case</p>
 
             </div>
         </div>
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </div>
 </body>
 </html>
